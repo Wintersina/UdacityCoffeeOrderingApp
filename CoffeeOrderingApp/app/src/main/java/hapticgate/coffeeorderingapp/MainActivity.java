668e945fa9e9c3b1ac1bfbde0577c08aa4b9d31a
Sina Serati <WinterSina@Sinas-MacBook-Pro.local>
2016-08-22 10:49:26 -0500

    updating the coffe order app to take method arguments

--- CoffeeOrderingApp/app/src/main/java/hapticgate/coffeeorderingapp/MainActivity.java
+++ CoffeeOrderingApp/app/src/main/java/hapticgate/coffeeorderingapp/MainActivity.java
@@ -1,8 +1,5 @@
 package hapticgate.coffeeorderingapp;
 
-import android.annotation.TargetApi;
-import android.icu.text.NumberFormat;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,29 +7,39 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int coffeeCounter = 0;
+    String cs_name = "Sina Serati";
+    int coffee_counter = 0;
     int cost = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+    }
+    private String customer_name(String name){
+        cs_name=name;
+        return cs_name;
+    }
+    private String create_order_summery( int counter){
+        String or_summery;
+        or_summery = "Name: "+ customer_name("Sina Serati");
+        or_summery +="\nTotal: $"+(coffee_counter *cost);
+        or_summery +="\nQuantity: "+coffee_counter+"\nThank you,";
+        return or_summery;
     }
     public void add_method(View view){
-        coffeeCounter= coffeeCounter +1;
-        display(coffeeCounter);
+        coffee_counter = coffee_counter +1;
+        display(coffee_counter);
     }
     public void sub_method(View view){
-        coffeeCounter= coffeeCounter -1;
-        display(coffeeCounter);
+        coffee_counter = coffee_counter -1;
+        display(coffee_counter);
     }
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.add_sub_text_view);
         quantityTextView.setText((""+number));
     }
-    public void order_method(View view){
+    public void order_summery_view(View view){
         TextView quantityTextView = (TextView) findViewById(R.id.total_text_view);
-        quantityTextView.setText((""+(coffeeCounter*cost)));
+        quantityTextView.setText(create_order_summery(coffee_counter));
     }
 }
